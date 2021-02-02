cbceb858ba7157e90295e519a83d59a039733cab
Punit Jain <punit.jain@simplybusiness.co.uk>
2021-02-02 16:13:57 +0000

    abstract configuration to a separate class

--- lib/action.rb
+++ lib/action.rb
@@ -1,6 +1,33 @@
 # frozen_string_literal: true
+require 'octokit'
 
 # Run action based on the command
 class Action
-  def update_version(label); end
+  attr_reader :client, :payload, :version_file_path, :repo
+
+  SEMVER_VERSION =
+    /["'](0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?["']/.freeze # rubocop:disable Layout/LineLength
+  GEMSPEC_VERSION = Regexp.new(/\.version\s*=\s*/.to_s + SEMVER_VERSION.to_s).freeze
+
+  def initialize(config)
+    @client = config.client
+    @payload = config.payload
+    @version_file_path = config.version_file_path
+    @repo = payload['repository']['full_name']
+  end
+
+  def update_version(label)
+    # get_version(ref: '')
+  end
+
+  def fetch_version(ref:)
+    content = client.contents(repo, path: version_file_path, query: { ref: ref })['content']
+    content = Base64.decode64(content)
+    version = content.match(GEMSPEC_VERSION) || content.match(SEMVER_VERSION)
+    format_version(version)
+  end
+
+  def format_version(version)
+    Gem::Version.new(version[0].split('=').last.gsub(/\s/, '').gsub(/\'|\"/, ''))
+  end
 end
