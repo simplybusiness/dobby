81780f0ee37de68802db34a05a79d9ec60ad2ba1
Punit Jain <punit.jain@simplybusiness.co.uk>
2021-02-03 17:36:38 +0000

    more tests

--- spec/lib/action_spec.rb
+++ spec/lib/action_spec.rb
@@ -23,6 +23,41 @@ describe Action do
 
   let(:action) { Action.new(config) }
 
+  describe '#updated_version_file' do
+    let(:content) { version_file_content('1.0.0') }
+
+    it 'bumps the major version' do
+      expected_content = version_file_content('2.0.0')
+      updated_content = action.updated_version_file(content, 'major')
+      expect(updated_content).to eq(expected_content)
+    end
+
+    it 'bumps the minor version' do
+      expected_content = version_file_content('1.1.0')
+      updated_content = action.updated_version_file(content, 'minor')
+      expect(updated_content).to eq(expected_content)
+    end
+
+    it 'bumps the patch version' do
+      expected_content = version_file_content('1.0.1')
+      updated_content = action.updated_version_file(content, 'patch')
+      expect(updated_content).to eq(expected_content)
+    end
+  end
+
+  describe '#fetch_content' do
+    it 'fetch the content for a given file on a branch' do
+      mock_version_response('1.0.0', 'master')
+      expect(client).to receive(:contents).with(
+        'simplybusiness/test',
+        path: 'lib/version.rb',
+        query: { ref: 'master' }
+      )
+      content = action.fetch_content(ref: 'master', path: 'lib/version.rb')
+      expect(content).to eq(version_file_content('1.0.0'))
+    end
+  end
+
   describe '#bump_version' do
     it 'add a comment for invalid semver' do
       expect(action).to receive(:add_comment_for_invalid_semver)
@@ -32,36 +67,13 @@ describe Action do
     it 'updates the version file with new version' do
       mock_version_response('1.0.0', 'master')
       updated_content = version_file_content('1.1.0')
-      expect(config.client).to receive(:update_contents).with(
+      expect(client).to receive(:update_contents).with(
         repo: 'simplybusiness/test',
         message: 'bump minor version',
         content: updated_content,
         branch: 'my_branch'
       )
       action.bump_version('minor')
-    end
-  end
-
-  describe '#updated_version_file' do
-    it 'bumps the major version' do
-      content = version_file_content('1.0.0')
-      expected_content = version_file_content('2.0.0')
-      updated_content = action.updated_version_file(content, 'major')
-      expect(updated_content).to eq(expected_content)
-    end
-
-    it 'bumps the minor version' do
-      content = version_file_content('1.0.0')
-      expected_content = version_file_content('1.1.0')
-      updated_content = action.updated_version_file(content, 'minor')
-      expect(updated_content).to eq(expected_content)
-    end
-
-    it 'bumps the patch version' do
-      content = version_file_content('1.0.0')
-      expected_content = version_file_content('1.0.1')
-      updated_content = action.updated_version_file(content, 'patch')
-      expect(updated_content).to eq(expected_content)
     end
   end
 
