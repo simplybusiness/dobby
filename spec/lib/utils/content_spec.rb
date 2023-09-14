9cb0938479a78767f6950e88eec1161ec5435d8e
Karan Shah <karan.shah@simplybusiness.co.uk>
2023-09-14 10:17:53 +0100

    Require content

--- spec/lib/utils/content_spec.rb
+++ spec/lib/utils/content_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../../spec_helper'
+require_relative '../../../lib/utils/content'
+
 describe Content do
   let(:client) { instance_double(Octokit::Client) }
   let(:config) do
