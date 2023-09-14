6fea7baf0a3edadc69b3eeb7e8f5a3d42decda5d
Karan Shah <karan.shah@simplybusiness.co.uk>
2023-09-14 09:42:50 +0100

    Let content be handled separately
    This has the advantage of making only one API call (vs. 2) and removes the responsibility away from the main action.rb.
    
    For now it just creates the new content, usage will come in a future PR.

---
+++ spec/lib/utils/content_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative '../../spec_helper'
+describe Content do
+  let(:client) { instance_double(Octokit::Client) }
+  let(:config) do
+    test_config = double
+    allow(test_config).to receive_messages(
+      client: client, version_file_path: 'lib/version.rb', payload: {
+        'repository' => { 'full_name' => repo_full_name },
+        'issue' => {
+          'number' => 1
+        },
+        'comment' => {
+          'id' => 123
+        }
+      })
+    test_config
+  end
+  let(:content) { Content.new(config: config, ref: 'master', path: 'lib/version.rb') }
+
+  it 'fetch the content for a given file on a branch' do
+    mock_version_response(client, '1.0.0', 'master')
+    expect(client).to receive(:contents).with(
+      repo_full_name,
+      path: 'lib/version.rb',
+      query: { ref: 'master' }
+    )
+    fetched_content = content.content
+    expect(fetched_content).to eq(version_file_content('1.0.0'))
+  end
+
+  it 'fetch the blob_sha for a given file on a branch' do
+    mock_version_response(client, '1.0.0', 'master')
+    expect(client).to receive(:contents).with(
+      repo_full_name,
+      path: 'lib/version.rb',
+      query: { ref: 'master' }
+    )
+    fetched_content = content.blob_sha
+    expect(fetched_content).to eq('abc1234')
+  end
+end
