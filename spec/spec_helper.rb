04c09145841dd9c759ed3e71f1129defa54d162a
Robert Peacock <robert.peacock@simplybusiness.co.uk>
2023-09-19 17:04:59 +0100

    drop 2.7 testing, upgrade target to 3.0, fix resulting cops

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-Dir["#{File.dirname(__FILE__)}/../lib/*.rb"].sort.each { |f| require_relative f }
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/../lib/*.rb"].each { |f| require_relative f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 require 'simplecov'
 SimpleCov.start
