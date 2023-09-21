895acc9ed2701619a7ee4933f4d2dabc52ded047
Karan Shah <karan.shah@simplybusiness.co.uk>
2023-09-21 11:03:42 +0100

    Refactor head branch ref

--- lib/utils/commit.rb
+++ lib/utils/commit.rb
@@ -14,12 +14,13 @@ class Commit
   end
 
   def multiple_files(files, commit_message)
-    ref = @client.ref(@repo, "heads/#{@head_branch}")
+    head_branch_ref = "heads/#{@head_branch}"
+    ref = @client.ref(@repo, head_branch_ref)
     current_sha = ref.object.sha
     current_tree = @client.commit(@repo, current_sha).commit.tree.sha
     new_tree = @client.create_tree(@repo, files, :base_tree => current_tree)
     new_commit = @client.create_commit(@repo, commit_message, new_tree.sha, current_sha)
     puts 'TEMP_KARAN', new_commit.sha
-    @client.update_ref(@repo, @head_branch, new_commit.sha)
+    @client.update_ref(@repo, head_branch_ref, new_commit.sha)
   end
 end
