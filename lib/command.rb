edc3d4bd851a889666cb68aa5697e32e56d078f6
Adam Leach <adam.leach@simplybusiness.co.uk>
2024-06-26 08:23:32 +0100

    Improve initialize method in command.rb

--- lib/command.rb
+++ lib/command.rb
@@ -8,19 +8,19 @@ class Command
 
   COMMAND_PREFIX = '/dobby'
 
-# Improved version of the initialize method in command.rb
-def initialize(config)
+  # Improved version of the initialize method in command.rb
+  def initialize(config)
     @config = config
     comment = config.payload['comment']['body'].strip.downcase
     unless comment.start_with?(COMMAND_PREFIX)
-        error_msg = "Comment must start with #{COMMAND_PREFIX}"
-        puts "::error title=Argument Error::#{error_msg}"
-        raise ArgumentError, error_msg
+      error_msg = "Comment must start with #{COMMAND_PREFIX}"
+      puts "::error title=Argument Error::#{error_msg}"
+      raise ArgumentError, error_msg
     end
 
     cmd = comment.delete_prefix(COMMAND_PREFIX).strip
     @command, @options = cmd.split(/\s+/, 2)
-end
+  end
 
   def call
     action = Action.new(config)
