a569e0b9fc920655d2204ad4999121bb9f244f2d
Adam Leach <adam.leach@simplybusiness.co.uk>
2024-10-18 16:52:48 +0100

    Update RuboCop configuration, bump dependencies, and enhance error messaging in action and command classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,4 +17,8 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before do
+    ENV['GITHUB_STEP_SUMMARY'] = 'temp.md'
+  end
 end
